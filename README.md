fe6b9c60eb3d6ed1b8949d3f52c0c46ccf063223
Rowan Merewood <merewood@google.com>
2020-12-03 04:58:13 -0500

    Initial retrofill example

---
+++ README.md
@@ -0,0 +1,27 @@
+# UA-CH retrofill
+
+This snippet illustrates how to reconstruct the legacy `navigator.userAgent` string value from the modern `navigator.userAgentData` values.
+
+## Usage instructions
+
+Copy the contents of `uach-retrofill.js` to the relevant location in your project.
+
+If you need any of the following values:
+
+* `platform` - the Operating System's name
+* `platformVersion` - the Operating System's version
+* `architecture` - the CPU architecture
+* `model` - the model on Android
+* `uaFullVersion` - the browser's full version, in case the significant version (provided by default) is not enough
+
+Then you will need to specify these as an array of strings to the parameter in `OverrideUserAgentUsingClientHints()` at the end of the file. For example, if you need the `platform` and `platformVersion`, you would specify:
+
+```js
+OverrideUserAgentUsingClientHints(["platform", "platformVersion"]).then(() => {
+  document.getElementById("output").innerHTML = navigator.userAgent;
+})
+```
+
+## Contributing
+
+Please see the [Code of Conduct](docs/code-of-conduct.md) and [Contributing](docs/contributing.md) guides.
---
+++ uach-retrofill.js
@@ -0,0 +1,153 @@
+/**
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This function's role is to enable smooth transition to the brave new world of User-Agent Client Hints.
+// If you have legacy code that relies on `navigator.userAgent` and which relies on entropy that will go away by default,
+// you *need* to refactor it to use UA-CH.
+// This function is to be used as a stop gap, to enable smooth transition during that period.
+const OverrideUserAgentUsingClientHints = hints => {
+  // Helper functions for platform specific strings
+  const GetCrosSpecificString = values => {
+    let newUA = "X11; CrOS ";
+    // Working around the lack of bitness value.
+    if (values.architecture == "x86") {
+      newUA += "x86_64";
+    } else {
+      newUA += "aarch64";
+    }
+    newUA += " ";
+    newUA += values.platformVersion;
+    return newUA;
+  }
+
+  const GetWindowsSpecificString = values => {
+    let newUA = "Windows NT ";
+    newUA += values.platformVersion;
+    newUA += "; ";
+    // Working around the lack of bitness value.
+    if (values.architecture == "x86") {
+      newUA += "x64";
+    } else {
+      // TODO: What's the UA string for windows over ARM??
+      newUA += "arm";
+    }
+    return newUA;
+  }
+
+  const GetMacSpecificString = values => {
+    let newUA = "Macintosh; Intel Mac OS X ";
+    newUA += values.platformVersion;
+    return newUA;
+  }
+
+  const GetAndroidSpecificString = values => {
+    let newUA = "Linux; Android ";
+    newUA += values.platformVersion;
+    if (values.model) {
+      newUA += "; ";
+      newUA += values.model;
+    }
+    return newUA;
+  }
+
+  const Initialize = (values, fallback_version) => {
+    if (!values.platform) {
+      values.platform = "Windows";
+    }
+    if (!values.platformVersion) {
+      values.platformVersion = "10.0";
+    }
+    if (!values.architecture) {
+      values.architecture = "x86";
+    }
+    if (!values.uaFullVersion) {
+      values.uaFullVersion = fallback_version + ".0.0.0";
+    }
+    if (!values.model) {
+      values.model = "";
+    }
+    return values;
+  }
+
+  if (!navigator.userAgentData || Date.now() > 1640995199000) {
+    return Promise.resolve();
+  }
+
+  // Verify that this is a Chromium
+  let is_chromium = false;
+  let is_edge = false
+  let chromium_version;
+  navigator.userAgentData.brands.forEach(value => {
+    if (value.brand == "Chromium") {
+      is_chromium = true;
+      chromium_version = value.version;
+    } else if (value.brand == "Microsoft Edge") {
+      is_edge = true;
+    }
+  });
+  if (!is_chromium) {
+    // If this is not a Chromium based browser, the UA string should be very different. So bailing...
+    return Promise.resolve();
+  }
+
+  // Main logic
+  return new Promise(resolve => {
+    navigator.userAgentData.getHighEntropyValues(hints).then(values => {
+      values = Initialize(values, chromium_version);
+      let newUA = "Mozilla/5.0 (";
+      if (values.platform == "Chrome OS") {
+        newUA += GetCrosSpecificString(values);
+      } else if (values.platform == "Windows") {
+        newUA += GetWindowsSpecificString(values);
+      } else if (values.platform == "Mac OS X") {
+        newUA += GetMacSpecificString(values);
+      } else if (values.platform == "Android") {
+        newUA += GetAndroidSpecificString(values);
+      } else {
+        newUA += "X11; Linux x86_64";
+      }
+      newUA += ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/";
+      newUA += values.uaFullVersion;
+      if (navigator.userAgentData.mobile) {
+        newUA += " Mobile";
+      }
+      newUA += " Safari/537.36";
+
+      if (is_edge) {
+        newUA += " Edg/";
+        // Note: The full version Edge includes is not the same as the equivalent Chrome full version
+        newUA += values.uaFullVersion;
+      }
+
+      // Got a new UA value. Now override `navigator.userAgent`.
+      Object.defineProperty(navigator, 'userAgent', {
+        value: newUA,
+        writable: false
+      });
+      resolve();
+    })
+  })
+}
+
+// Modify the input array below to include the values you *really* need
+// platform - provides the Operating System's name
+// platformVersion - provides the Operating System's version
+// architecture - provides the CPU architecture
+// model - provides the model on Android
+// uaFullVersion - provides the browser's full version, in case the significant version (provided by default) is not enough
+OverrideUserAgentUsingClientHints([""]).then(() => {
+  document.getElementById("output").innerHTML = navigator.userAgent;
+})
