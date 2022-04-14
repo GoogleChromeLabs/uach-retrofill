ee2bc13185556fc6d139cf3b75946c3c96d7d4ec
Mike Taylor <miketaylr@chromium.org>
2022-04-14 09:28:00 -0400

    Issue #34 - clean up wrapping, quotes, semicolons

--- uach-retrofill.js
+++ uach-retrofill.js
@@ -28,72 +28,72 @@
 */
 async function getUserAgentUsingClientHints(hints) {
   // Helper functions for platform specific strings
-  const GetCrosSpecificString = values => {
-    let osCPUFragment = '';
-    if (values.bitness == '64') {
-      if (values.architecture == 'x86') {
-        osCPUFragment = 'x86_64'
-      } else if (values.architecture == 'arm') {
-        osCPUFragment = 'aarch64';
+  const GetCrosSpecificString = (values) => {
+    let osCPUFragment = "";
+    if (values.bitness == "64") {
+      if (values.architecture == "x86") {
+        osCPUFragment = "x86_64";
+      } else if (values.architecture == "arm") {
+        osCPUFragment = "aarch64";
       }
-    } else if (values.architecture == 'arm' && values.bitness == '32') {
-      osCPUFragment = 'armv7l';
+    } else if (values.architecture == "arm" && values.bitness == "32") {
+      osCPUFragment = "armv7l";
     }
-    if (osCPUFragment == '') {
+    if (osCPUFragment == "") {
       return `X11; CrOS ${values.platformVersion}`;
     }
     return `X11; CrOS ${osCPUFragment} ${values.platformVersion}`;
-  }
+  };
 
-  const GetWindowsSpecificString = values => {
-    let osCPUFragment = '';
-    if (values.architecture == 'x86' && values.bitness == '64') {
-      osCPUFragment = '; Win64; x64';
-    } else if (values.architecture == 'arm'){
-      osCPUFragment = '; ARM';
+  const GetWindowsSpecificString = (values) => {
+    let osCPUFragment = "";
+    if (values.architecture == "x86" && values.bitness == "64") {
+      osCPUFragment = "; Win64; x64";
+    } else if (values.architecture == "arm") {
+      osCPUFragment = "; ARM";
     }
     return `Windows NT ${values.platformVersion}${osCPUFragment}`;
-  }
+  };
 
-  const GetMacSpecificString = values => {
-    let newUA = 'Macintosh; Intel Mac OS X ';
+  const GetMacSpecificString = (values) => {
+    let newUA = "Macintosh; Intel Mac OS X ";
     let macVersion = values.platformVersion;
-    if (macVersion.indexOf('.') > -1) {
-      macVersion = macVersion.split('.').join('_');
+    if (macVersion.indexOf(".") > -1) {
+      macVersion = macVersion.split(".").join("_");
     }
     newUA += macVersion;
     return newUA;
-  }
+  };
 
-  const GetAndroidSpecificString = values => {
-    let newUA = 'Linux; Android ';
+  const GetAndroidSpecificString = (values) => {
+    let newUA = "Linux; Android ";
     newUA += values.platformVersion;
     if (values.model) {
-      newUA += '; ';
+      newUA += "; ";
       newUA += values.model;
     }
     return newUA;
-  }
-
+  };
 
   const Initialize = (values) => {
     if (!values.architecture) {
-      values.architecture = 'x86';
+      values.architecture = "x86";
     }
     if (!values.bitness) {
-      values.bitness = '64';
+      values.bitness = "64";
     }
     if (!values.model) {
-      values.model = '';
+      values.model = "";
     }
     if (!values.platform) {
-      values.platform = 'Windows';
+      values.platform = "Windows";
     }
     if (!values.platformVersion) {
-      values.platformVersion = '10.0';
+      values.platformVersion = "10.0";
     }
+
     return values;
-  }
+  };
 
   if (!navigator.userAgentData || Date.now() > 1640995199000) {
     return Promise.resolve();
@@ -102,15 +102,17 @@ async function getUserAgentUsingClientHints(hints) {
   // Verify that this is a Chromium-based browser
   let is_chromium = false;
   let chromium_version;
-  const is_chrome_ua_pattern = new RegExp('AppleWebKit/537.36 \\(KHTML, like Gecko\\) Chrome/\\d+.\\d+.\\d+.\\d+ (Mobile )?Safari/537.36$');
-  navigator.userAgentData.brands.forEach(value => {
-    if (value.brand == 'Chromium') {
+  const is_chrome_ua_pattern = new RegExp(
+    "AppleWebKit/537.36 \\(KHTML, like Gecko\\) Chrome/\\d+.\\d+.\\d+.\\d+ (Mobile )?Safari/537.36$"
+  );
+  navigator.userAgentData.brands.forEach((value) => {
+    if (value.brand == "Chromium") {
       // Let's double check the UA string as well, so we don't accidentally
       // capture a headless browser or friendly bot (which should report as
       // HeadlessChrome or something entirely different).
       is_chromium = is_chrome_ua_pattern.test(navigator.userAgent);
       chromium_version = value.version;
-    } 
+    }
   });
   if (!is_chromium) {
     // If this is not a Chromium-based browser, the UA string should be very
@@ -119,53 +121,53 @@ async function getUserAgentUsingClientHints(hints) {
   }
 
   // Main logic
-  return new Promise(resolve => {
-    navigator.userAgentData.getHighEntropyValues(hints).then(values => {
+  return new Promise((resolve) => {
+    navigator.userAgentData.getHighEntropyValues(hints).then((values) => {
       let initialValues = {
         platform: navigator.userAgentData?.platform,
         uaFullVersion: `${chromium_version}.0.0.0`,
       };
       values = Object.assign(initialValues, values);
       values = Initialize(values);
-      let newUA = 'Mozilla/5.0 (';
-      if (['Chrome OS', 'Chromium OS'].includes(values.platform)) {
+      let newUA = "Mozilla/5.0 (";
+      if (["Chrome OS", "Chromium OS"].includes(values.platform)) {
         newUA += GetCrosSpecificString(values);
-      } else if (values.platform == 'Windows') {
+      } else if (values.platform == "Windows") {
         newUA += GetWindowsSpecificString(values);
-      } else if (values.platform == 'macOS') {
+      } else if (values.platform == "macOS") {
         newUA += GetMacSpecificString(values);
-      } else if (values.platform == 'Android') {
+      } else if (values.platform == "Android") {
         newUA += GetAndroidSpecificString(values);
       } else {
-        newUA += 'X11; Linux x86_64';
+        newUA += "X11; Linux x86_64";
       }
-      newUA += ') AppleWebKit/537.36 (KHTML, like Gecko) Chrome/';
+      newUA += ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/";
       newUA += values.uaFullVersion;
       if (navigator.userAgentData.mobile) {
-        newUA += ' Mobile';
+        newUA += " Mobile";
       }
-      newUA += ' Safari/537.36';
+      newUA += " Safari/537.36";
       resolve(newUA);
-    })
-  })
+    });
+  });
 }
 
 /**
-* @param {string[]} hints
-* @return {Promise<string|undefined>} A Promise that resolves on overriding the
-*   navigator.userAgent string.
-*/
+ * @param {string[]} hints
+ * @return {Promise<string|undefined>} A Promise that resolves on overriding the
+ *   navigator.userAgent string.
+ */
 async function overrideUserAgentUsingClientHints(hints) {
-  return new Promise(resolve => {
-    getUserAgentUsingClientHints(hints).then(newUA => {
+  return new Promise((resolve) => {
+    getUserAgentUsingClientHints(hints).then((newUA) => {
       // Got a new UA value. Now override `navigator.userAgent`.
-      Object.defineProperty(navigator, 'userAgent', {
+      Object.defineProperty(navigator, "userAgent", {
         value: newUA,
         writable: false,
-        configurable: true
+        configurable: true,
       });
       resolve();
-    })
+    });
   });
 }
 
